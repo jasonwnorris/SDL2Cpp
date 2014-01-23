07c4746efacde9b11f244a805853c90f0093a6a2
Jason Norris <bakkon@gmail.com>
2014-01-23 17:05:06 -0500

    Laid out extension libraries
    Audio and Font added

---
+++ SDL2Cpp/SDL2Cpp/include/SDL2Cpp/Font/Font.hpp
@@ -0,0 +1,33 @@
+// Font.hpp
+
+#ifndef __SDL2_FONT_FONT_H__
+#define __SDL2_FONT_FONT_H__
+
+/*
+
+// SDL Includes
+#include <SDL_ttf.h>
+
+namespace SDL2
+{
+	namespace Font
+	{		
+		int Initialize()
+		{
+			int code = TTF_init();
+			if(code < 0)
+				SDL_Log("Failed initialize SDL2_Ttf: %s", TTF_GetError());
+
+			return code;
+		}
+
+		void Finalize()
+		{
+			TTF_Quit();
+		}
+	}
+}
+
+*/
+
+#endif
